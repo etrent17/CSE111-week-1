2f20506fa7e20f0f0e232b9ab360bfe48ce23374
Elijah <trent6679@gmail.com>
2023-01-05 17:59:08 -0700

    first init

---
+++ heart.py
@@ -0,0 +1,10 @@
+"""Write a Python program named heart_rate.py that asks for a persons age and 
+computes and outputs the slowest and fastest rates necessary to strengthen his 
+heart. To start your program, copy and paste the following code into your program 
+and use it as an outline as you write code. Note that in a Python program, a triple 
+quoted string at the top of the file acts as a comment for the entire program.
+"""
+
+user_age = int(input("What is your age? "))
+
+heart_max = 220 - user_age
